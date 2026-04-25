ec137ea0972a6bd540c88ecba302606f84cf6baf
Pushpit1 <81951634+Pushpit1@users.noreply.github.com>
2026-04-25 14:28:44 +0530

    cm

--- EST_PRACTICAL/LEETCODE_3 .cpp
+++ EST_PRACTICAL/LEETCODE_3 .cpp
@@ -1,26 +1,18 @@
 class Solution {
     public:
+        int lengthOfLongestSubstring(string s) {
+            unordered_set<char> st;
+            int left = 0, maxLen = 0;
     
-      void quickSort(vector<int>& arr, int low, int high) {
-          if (low < high) {
-              int p = partition(arr, low, high);
-              quickSort(arr, low, p - 1);
-              quickSort(arr, p + 1, high);
-          }
-      }
-  
-      int partition(vector<int>& arr, int low, int high) {
-          int pivot = arr[high];
-          int i = low - 1;
-  
-          for (int j = low; j < high; j++) {
-              if (arr[j] <= pivot) {
-                  i++;
-                  swap(arr[i], arr[j]);
-              }
-          }
-  
-          swap(arr[i + 1], arr[high]);
-          return i + 1;
-      }
-  };
+            for (int right = 0; right < s.size(); right++) {
+                while (st.count(s[right])) {
+                    st.erase(s[left]);
+                    left++;
+                }
+                st.insert(s[right]);
+                maxLen = max(maxLen, right - left + 1);
+            }
+    
+            return maxLen;
+        }
+    };
